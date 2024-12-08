92f1d30b1c23b494b225fd00f274a7c63b7ed8a9
Eskinder <Eskinderytbarek323@gmail.com>
2024-12-09 00:06:36 +0300

    first commit

--- Button.jsx
+++
@@ -1,8 +0,0 @@
-function Button() {
-    const handleClick = (e) => e.target.textContent = "OUCH! 🤕";
-  
-    return <button onDoubleClick={handleClick}>Click me 😁</button>;
-  }
-  
-  export default Button;
-  
